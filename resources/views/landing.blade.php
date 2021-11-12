2c6bf6003ac34acc083232ccf9f4ecf94a92f257
orangeseeds <58601720+orangeseeds@users.noreply.github.com>
2021-11-12 06:53:26 +0545

    missed a thing

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -4,6 +4,8 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
+  <script type="text/javascript" src="{{ asset('js/app.js') }}" defer></script>
+<script type="text/javascript" src="{{ asset('js/main.js') }}" defer></script>
   <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link rel="stylesheet" href="{{ asset('css/landing.css') }}">
